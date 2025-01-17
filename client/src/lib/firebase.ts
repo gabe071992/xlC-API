08235d836583aaf6166fc92d2c173ad4d7f9d4da
gabrieltgideon <36413854-gabrieltgideon@users.noreply.replit.com>
2025-01-17 01:04:27 +0000

    User checkpoint: Update firebase import path.

--- client/src/lib/firebase.ts
+++ client/src/lib/firebase.ts
@@ -1,4 +1,4 @@
-import { auth, db as database } from '../../server/config/firebase';
+import { auth, db as database } from '../server/config/firebase.ts';
 
 export { auth, database };
 export default { auth, database };
